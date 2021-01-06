902edfb1aadedba866b80baceb5930077d605937
Emma-Tinpan <c_uav@yahoo.co.jp>
2021-01-06 22:16:56 +0900

    データ解析_キックオフ

---
+++ Data-analysis.py
@@ -0,0 +1,13 @@
+import pandas
+from sklearn.datasets import load_iris
+
+iris = load_iris()
+iris_pd = pandas.DataFrame(iris.data, columns=iris.feature_names)
+iris_target_pd = pandas.DataFrame(iris.target)
+
+print(iris_pd.head())
+print(iris_target_pd.head())
+
+#join（横方向）
+iris_data = pandas.concat([iris_pd,iris_target_pd],axis=1)
+print(iris_data)
