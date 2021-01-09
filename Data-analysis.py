a399897568a5b47637696301c774114dab3f118f
Emma-Tinpan <c_uav@yahoo.co.jp>
2021-01-09 20:51:58 +0900

    pythonデータ分割

--- Data-analysis.py
+++ Data-analysis.py
@@ -1,5 +1,9 @@
 import pandas
 from sklearn.datasets import load_iris
+import random
+import numpy
+
+#from sklearn import cluster
 
 iris = load_iris()
 iris_pd = pandas.DataFrame(iris.data, columns=iris.feature_names)
@@ -17,3 +21,22 @@ print(iris_data)
 print(iris_data.dtypes)
 #pandasの.describe()
 print(iris_data.describe())
+
+#データの行をランダムに入れ替える
+#pandas.dataframe.sample()
+#frac=1でシャッフルできる
+#reset_index()メソッドでデータフレームインデックスをリセットする
+data_shuffle_1 = iris_data.sample(frac=1).reset_index(drop=True)
+print(data_shuffle_1)
+
+#numpy.random.permutation()　DaraFrameのインデックスをシャッフルする
+#iloc()メソッドを使用するとランダムにできる
+data_shuffle_2 = iris_data.iloc[numpy.random.permutation(iris_data.index)].reset_index(drop=True)
+print(data_shuffle_2)
+
+#分割0~120 120~150に分ける
+data1 = data_shuffle_1[:120]
+data2 = data_shuffle_1[120:]
+
+print(data1)
+print(data2)
