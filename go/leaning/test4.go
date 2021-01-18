dbefd32db4430f1f6ee1cc31b8069d5b42c0b0f4
Emma-Tinpan <c_uav@yahoo.co.jp>
2021-01-18 23:18:00 +0900

    Go 勉強

---
+++ go/leaning/test4.go
@@ -0,0 +1,25 @@
+package leaning
+
+import {
+	"fmt"
+}
+
+/* func [関数名] ([引数の定義]) [戻り値型]{
+	[関数本体]
+} */
+
+func plus (x, y int) int {
+	return x + y
+}
+
+//複数の戻り値を返す方法
+func div(a, b int) (int, int) {
+	q := a / b
+	r := a % b
+	return q,r
+}
+
+func main() {
+	q, r := div (19, 7)
+	fmt.Printf("商=%d 余剰=%d\n", q, r)
+}
